ddecb6e0613b534496f48cfff093139a2fc5a554
Royal Pope <rvssianby@gmail.com>
2023-04-24 19:31:58 +0200

    ok

---
+++ js.js
@@ -0,0 +1,41 @@
+// let x = 3
+// let y = 4
+// let sum =x+y
+// console.log(sum);
+
+
+// for(var i=0; i<=10; i++){
+//     console.log(i);
+//     if(i=5){
+//     console.log(i);
+//     }
+// }
+
+// function evening(){
+//     console.log("good evening");
+// }
+// var btn= document.getElementById("btn");
+// btn.addEventListener('click', (e) =>{
+//     e.preventDefault();
+//     var one = parseInt(document.getElementById("one").value);
+//     var two = parseInt(document.getElementById("two").value);
+//     var sub=one-two;
+//     console.log(sub);
+// })
+// function sum(){
+
+// }
+
+tm.addEventListener('click', () => {
+
+var hr = parseInt(document.getElementById("time").value);
+
+if (hr>=6 && hr < 12) {
+    alert('Good morning');
+}
+else if (hr >= 12 && hr < 18){
+    alert('Good Afternoon');
+}
+else
+    alert('good evening');
+})
