807a02d2adad31cb60ff41e7564d599190c85f5b
Royal Pope <rvssianby@gmail.com>
2023-04-27 20:28:40 +0200

    yahuuuw

--- js.js
+++ js.js
@@ -44,9 +44,31 @@
 // p.innerHTML = "Liverpool";
 // myDiv.append(p)
 
-function change(){
-    document.getElementById("pic").src="gtr2.jpg";
-}
-function back(){
-        document.getElementById("pic").src="gtr1.jpg";
-}
+// function change(){
+//     document.getElementById("pic").src="gtr2.jpg";
+// }
+// function back(){
+//         document.getElementById("pic").src="gtr1.jpg";
+// }
+const btn = document.getElementById('btn');
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+// var label = document.getElementById("label");
+// var msg = document.getElementById("msg");
+// function validateEmail(){
+//     if(!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+//         msg.innerHTML = "Please enter a valid email";
+//         return false;
+//     }
+// }
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    var email_value = document.getElementById('one').value;
+    var length = email_value.length;
+    if(length < 6) {
+        email.classList.add("active");
+    }
+    if(length < 8){
+        password.classList.add("active");
+    }
+})
